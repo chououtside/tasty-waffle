eb1e9e655da550eeddd1b160ca55bc83ead360a4
chououtside <chouming3@gmail.com>
2017-05-11 10:29:27 -0400

    Add Dependencies to Package JSON and Create Server
    Server runs index.html page from client
    
     Changes to be committed:
    	modified:   package.json
    	new file:   server/server.js

---
+++ server/server.js
@@ -0,0 +1,24 @@
+var express = require('express');
+
+// Middleware
+var morgan = require('morgan');
+var parser = require('body-parser');
+
+var app = express();
+module.exports.app = app;
+
+// Set what we are listening on.
+app.set('port', 2000);
+
+app.use(morgan('dev'));
+app.use(parser.urlencoded({extended: true}));
+app.use(parser.json());
+
+// Serve the client files
+app.use(express.static(__dirname + '/../client'));
+
+// If we are being run directly, run the server.
+if (!module.parent) {
+  app.listen(app.get('port'));
+  console.log('Listening on', app.get('port'));
+}
