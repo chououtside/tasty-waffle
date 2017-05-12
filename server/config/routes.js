af24a5379d91743e2e14d06996e174c9a05a5965
chououtside <chouming3@gmail.com>
2017-05-12 14:39:45 -0400

    Add Semicolins
     Changes to be committed:
    	modified:   server/config/routes.js

--- server/config/routes.js
+++ server/config/routes.js
@@ -1,6 +1,6 @@
 var router = require('express').Router();
-var userController = require('../users/userController')
-var dayController = require('../day/dayController')
+var userController = require('../users/userController');
+var dayController = require('../day/dayController');
 
 router.get('/api/days', dayController.findDays);
 router.get('/api/users', userController.findUsers);
