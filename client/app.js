e5361dfeb90609e3c890b22d45aad33211ed11e8
chououtside <chouming3@gmail.com>
2017-05-13 12:44:22 -0400

    Create App File for Angular App
    Add ng-route and setup route for main path
    Add Days Controller module to the app
    
     Changes to be committed:
    	new file:   client/app.js

---
+++ client/app.js
@@ -0,0 +1,12 @@
+angular.module('shiftmanager', ['ngRoute', 'shiftmanager.days'])
+.config(['$locationProvider', function($locationProvider) {
+  $locationProvider.hashPrefix('');
+}])
+.config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
+  $locationProvider.hashPrefix('');
+  $routeProvider
+    .when('/', {
+      templateUrl: 'days/days.html',
+      controller: 'daysController'
+    });
+}])
