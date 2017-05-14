3fb6b056c9aac1bcd9dab224a217b36c041d4cb5
chououtside <chouming3@gmail.com>
2017-05-14 04:44:58 -0400

    Create AddStaff Function in Day Service
     Changes to be committed:
    	modified:   client/services.js

--- client/services.js
+++ client/services.js
@@ -10,7 +10,23 @@ angular.module('services', [])
     });
   };
 
+  var addStaff = function(day, staffArray, shift) {
+    return $http({
+      method: 'PUT',
+      url: 'api/' + day + '/staff',
+      data: {
+        day: day,
+        staffArray: staffArray,
+        shift: shift
+      }
+    })
+    .then(function(resp) {
+      return resp.data;
+    });
+  };
+
   return {
-    initializeDays: initializeDays
-  }
+    initializeDays: initializeDays,
+    addStaff: addStaff
+  };
 });
